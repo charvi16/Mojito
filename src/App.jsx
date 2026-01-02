543172c73389c030be3a8d29c0dd5f57c72807cf
Charvi Agarwal <charviagarwal@Charvis-MacBook-Pro.local>
2026-01-02 19:00:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ScrollTrigger, SplitText } from 'gsap/all';
+//will probobly have to use splittext from gsap-trial/all (its paid)😩
+import gsap from 'gsap';
+import './App.css'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export default function App() {
+  return (
+    <>
+       
+    </>
+  );
+}
+
