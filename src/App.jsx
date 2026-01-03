48eca37d3da36f8e715577e004b347287ed68867
Charvi Agarwal <charviagarwal@Charvis-MacBook-Pro.local>
2026-01-03 14:33:10 +0530

    Implement about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from 'gsap';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Cocktail from './components/Cocktail';
+import About from './components/About';
 import './App.css'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -14,6 +15,7 @@ export default function App() {
        <Navbar/>
        <Hero/>
        <Cocktail/>
+       <About/>
     </main>
   );
 }
